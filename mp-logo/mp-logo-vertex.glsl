8de28d9c820a8980003dd5216b13b5ef21d173a9
williamcheng <csh1234_1@126.com>
2023-09-23 11:48:40 -0500

    complete mp-logo

--- mp-logo/mp-logo-vertex.glsl
+++ mp-logo/mp-logo-vertex.glsl
@@ -11,20 +11,20 @@ void main() {
     vColor = color;
 
     // Zoom
-    float scaledCos = 1.0 - (0.5 * cos(seconds * 1.8) + 0.5) * (0.5 * cos(seconds * 1.8) + 0.5);
-    float zoomFactor = 0.6 * scaledCos + 0.4;
+    float scaledCos = 1.0 - (0.5 * cos(seconds * 2.5) + 0.5) * (0.5 * cos(seconds * 2.5) + 0.5);
+    float zoomFactor = 0.3 * scaledCos + 0.4;
     vec2 zoomedPosition = position.xy * zoomFactor;
     
     // Rotate
     vec2 rotatedPosition = zoomedPosition;
     float t = seconds;
-    rotatedPosition.x = 0.6*(zoomedPosition.x * cos(t) - zoomedPosition.y * sin(t));
-    rotatedPosition.y = 0.6*(zoomedPosition.x * sin(t) + zoomedPosition.y * cos(t));
+    rotatedPosition.x = 0.6*(zoomedPosition.x * cos(0.5*t) - zoomedPosition.y * sin(0.5*t));
+    rotatedPosition.y = 0.6*(zoomedPosition.x * sin(0.5*t) + zoomedPosition.y * cos(0.5*t));
 
     // Translate
     vec2 translation = vec2(
-        0.3*sin(0.5*t),
-        0.3*cos(0.5*t)
+        0.3*sin(1.2*t),
+        0.3*cos(1.2*t)
     );
     vec2 translatedPosition = rotatedPosition + translation;
     
